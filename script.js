64de2176d0c114568bb8166fb71d2e89cd472393
drianoK <drianokvcs@gmail.com>
2023-02-07 19:35:40 +0100

    Make slide effect function to mobile navigation__button -- Animate navigation_icon if click navigation__button

---
+++ script.js
@@ -0,0 +1,24 @@
+const checkBox = document.getElementById("navi-toggle");
+const navBackGround = document.getElementById("background-menu")
+let = menuIsOpen = false;
+
+
+
+checkBox.addEventListener("click", () => {
+    if(!menuIsOpen) {
+        navBackGround.classList.add("menu-active");
+        menuIsOpen = true;
+    } else {
+        navBackGround.classList.remove("menu-active");
+        menuIsOpen = false;
+    }
+
+})
+
+
+
+// checkBox.addEventListener("click", () => {
+//     navBackGround.classList.toggle("menu-active");
+// })
+
+
