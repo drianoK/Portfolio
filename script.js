84fb83d973beb85e18a5983afbefb09b62265791
drianoK <drianokvcs@gmail.com>
2023-02-12 14:08:06 +0100

    Changed const checkBox name to navBtn and added to a function this new const name on line 7. - - Added new ID to navigatio__button in index.html

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const checkBox = document.getElementById("navi-toggle");
+const navBtn = document.getElementById("navi-toggle");
 const navBackGround = document.getElementById("background-menu");
 let = menuIsOpen = false;
 
 
 
-checkBox.addEventListener("click", () => {
+navBtn.addEventListener("click", () => {
     if(!menuIsOpen) {
         navBackGround.classList.add("menu-active");
         menuIsOpen = true;
