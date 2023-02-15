71eef621cfb4dd5570f0888fa0ec43229ac5a30c
drianoK <drianokvcs@gmail.com>
2023-02-15 21:08:33 +0100

    Added to mobile navigation__button active and visited property in navigation.scss - - Added space betweencurly brackets in script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const navBtn = document.getElementById("navi-toggle");
 const navBackGround = document.getElementById("background-menu");
 const btnIcon = document.querySelector(".navigation__icon");
-const moveToSection = document.querySelectorAll(".navbar__item")
+const moveToSection = document.querySelectorAll(".navbar__item");
 let = menuIsOpen = false;
 
 
 
 navBtn.addEventListener("click", () => {
-    if(!menuIsOpen) {
-        navBackGround.classList.add("menu-active");
-        btnIcon.classList.add("navigation__icon--hamburger");
-        menuIsOpen = true;
-    } else {
-        navBackGround.classList.remove("menu-active");
-        btnIcon.classList.remove("navigation__icon--hamburger");
-        menuIsOpen = false;
+        if(!menuIsOpen) {
+            navBackGround.classList.add("menu-active");
+            btnIcon.classList.add("navigation__icon--hamburger");
+            menuIsOpen = true;
+        } else {
+            navBackGround.classList.remove("menu-active");
+            btnIcon.classList.remove("navigation__icon--hamburger");
+            menuIsOpen = false;
+        }
     }
-
-});
+);
 
 
 moveToSection.forEach(n => n.addEventListener("click", () => {
         navBackGround.classList.remove("menu-active");
         btnIcon.classList.remove("navigation__icon--hamburger");
-}))
-
-
+        }
+    )
+);
 
